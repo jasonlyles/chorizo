6e25116953c491d6be66a80bb33c0d5114ecc057
Christopher Sexton <csexton@gmail.com>
2009-09-22 15:33:51 -0400

    Setting up db and deploy

--- Capfile
+++ Capfile
@@ -1,26 +1,3 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
-
-default_run_options[:pty] = true
-
-# be sure to change these
-set :domain, 'districttaco.com'
-set :application, 'dtaco'
-
-# the rest should be good
-set :repository,  "git@github.com:csexton/chorizo.git"
-set :deploy_to, "/home/dtaco/dev.districttaco.com"
-set :deploy_via, :remote_cache
-set :scm, 'git'
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :use_sudo, false
-
-server domain, :app, :web
-
-namespace :deploy do
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
-
+Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+load 'config/deploy'
---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+default_run_options[:pty] = true
+
+# Site settings
+set :domain, 'districttaco.com'
+set :application, 'dtaco'
+
+set :repository,  "git@github.com:csexton/chorizo.git"
+set :deploy_to, "/home/dtaco/dev.districttaco.com"
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+after "deploy:update_code", "db:symlink"
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+namespace :db do
+ desc "Make symlink for database file and config file"
+  task :symlink do
+    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  end
+end
+
+
